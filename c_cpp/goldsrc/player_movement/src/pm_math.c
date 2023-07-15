295a4737eb7635a63c8bd8a2da7af603270c9813
Vesper <3692034+noodlecollie@users.noreply.github.com>
2023-07-15 16:36:49 +0100

    Trimmed down maths functions

--- c_cpp/goldsrc/player_movement/src/pm_math.c
+++ c_cpp/goldsrc/player_movement/src/pm_math.c
@@ -27,16 +27,8 @@
 
 #pragma warning(disable : 4244)
 
-#ifndef DISABLE_VEC_ORIGIN
-pmvec3_t vec3_origin = {0, 0, 0};
-#endif
-int nanmask = 255 << 23;
-
-float anglemod(float a)
-{
-	a = (360.0 / 65536) * ((int)(a * (65536 / 360.0)) & 65535);
-	return a;
-}
+const pmvec3_t vec3_origin = {0, 0, 0};
+const int nanmask = 255 << 23;
 
 void AngleVectors(const pmvec3_t angles, pmvec3_t forward, pmvec3_t right, pmvec3_t up)
 {
@@ -108,153 +100,6 @@ void AngleVectorsTranspose(const pmvec3_t angles, pmvec3_t forward, pmvec3_t rig
 	}
 }
 
-#ifndef DISABLE_VEC_FUNCS
-void AngleMatrix(const pmvec3_t angles, float (*matrix)[4])
-{
-	float angle;
-	float sr, sp, sy, cr, cp, cy;
-
-	angle = angles[YAW] * (M_PI * 2 / 360);
-	sy = sin(angle);
-	cy = cos(angle);
-	angle = angles[PITCH] * (M_PI * 2 / 360);
-	sp = sin(angle);
-	cp = cos(angle);
-	angle = angles[ROLL] * (M_PI * 2 / 360);
-	sr = sin(angle);
-	cr = cos(angle);
-
-	// matrix = (YAW * PITCH) * ROLL
-	matrix[0][0] = cp * cy;
-	matrix[1][0] = cp * sy;
-	matrix[2][0] = -sp;
-	matrix[0][1] = sr * sp * cy + cr * -sy;
-	matrix[1][1] = sr * sp * sy + cr * cy;
-	matrix[2][1] = sr * cp;
-	matrix[0][2] = (cr * sp * cy + -sr * -sy);
-	matrix[1][2] = (cr * sp * sy + -sr * cy);
-	matrix[2][2] = cr * cp;
-	matrix[0][3] = 0.0;
-	matrix[1][3] = 0.0;
-	matrix[2][3] = 0.0;
-}
-
-void AngleIMatrix(const pmvec3_t angles, float matrix[3][4])
-{
-	float angle;
-	float sr, sp, sy, cr, cp, cy;
-
-	angle = angles[YAW] * (M_PI * 2 / 360);
-	sy = sin(angle);
-	cy = cos(angle);
-	angle = angles[PITCH] * (M_PI * 2 / 360);
-	sp = sin(angle);
-	cp = cos(angle);
-	angle = angles[ROLL] * (M_PI * 2 / 360);
-	sr = sin(angle);
-	cr = cos(angle);
-
-	// matrix = (YAW * PITCH) * ROLL
-	matrix[0][0] = cp * cy;
-	matrix[0][1] = cp * sy;
-	matrix[0][2] = -sp;
-	matrix[1][0] = sr * sp * cy + cr * -sy;
-	matrix[1][1] = sr * sp * sy + cr * cy;
-	matrix[1][2] = sr * cp;
-	matrix[2][0] = (cr * sp * cy + -sr * -sy);
-	matrix[2][1] = (cr * sp * sy + -sr * cy);
-	matrix[2][2] = cr * cp;
-	matrix[0][3] = 0.0;
-	matrix[1][3] = 0.0;
-	matrix[2][3] = 0.0;
-}
-#endif
-
-void NormalizeAngles(float* angles)
-{
-	int i;
-	// Normalize angles
-	for ( i = 0; i < 3; i++ )
-	{
-		if ( angles[i] > 180.0 )
-		{
-			angles[i] -= 360.0;
-		}
-		else if ( angles[i] < -180.0 )
-		{
-			angles[i] += 360.0;
-		}
-	}
-}
-
-/*
-===================
-InterpolateAngles
-
-Interpolate Euler angles.
-FIXME:  Use Quaternions to avoid discontinuities
-Frac is 0.0 to 1.0 ( i.e., should probably be clamped, but doesn't have to be )
-===================
-*/
-void InterpolateAngles(float* start, float* end, float* output, float frac)
-{
-	int i;
-	float ang1, ang2;
-	float d;
-
-	NormalizeAngles(start);
-	NormalizeAngles(end);
-
-	for ( i = 0; i < 3; i++ )
-	{
-		ang1 = start[i];
-		ang2 = end[i];
-
-		d = ang2 - ang1;
-		if ( d > 180 )
-		{
-			d -= 360;
-		}
-		else if ( d < -180 )
-		{
-			d += 360;
-		}
-
-		output[i] = ang1 + d * frac;
-	}
-
-	NormalizeAngles(output);
-}
-
-/*
-===================
-AngleBetweenVectors
-
-===================
-*/
-float AngleBetweenVectors(const pmvec3_t v1, const pmvec3_t v2)
-{
-	float angle;
-	float l1 = Length(v1);
-	float l2 = Length(v2);
-
-	if ( !l1 || !l2 )
-		return 0.0f;
-
-	angle = acos(DotProduct(v1, v2)) / (l1 * l2);
-	angle = (angle * 180.0f) / M_PI;
-
-	return angle;
-}
-
-#ifndef DISABLE_VEC_FUNCS
-void VectorTransform(const pmvec3_t in1, float in2[3][4], pmvec3_t out)
-{
-	out[0] = DotProduct(in1, in2[0]) + in2[0][3];
-	out[1] = DotProduct(in1, in2[1]) + in2[1][3];
-	out[2] = DotProduct(in1, in2[2]) + in2[2][3];
-}
-
 int VectorCompare(const pmvec3_t v1, const pmvec3_t v2)
 {
 	int i;
@@ -272,46 +117,16 @@ void VectorMA(const pmvec3_t veca, float scale, const pmvec3_t vecb, pmvec3_t ve
 	vecc[1] = veca[1] + scale * vecb[1];
 	vecc[2] = veca[2] + scale * vecb[2];
 }
-#endif
 
-pmvec_t _DotProduct(pmvec3_t v1, pmvec3_t v2)
-{
-	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
-}
-
-void _VectorSubtract(pmvec3_t veca, pmvec3_t vecb, pmvec3_t out)
-{
-	out[0] = veca[0] - vecb[0];
-	out[1] = veca[1] - vecb[1];
-	out[2] = veca[2] - vecb[2];
-}
-
-void _VectorAdd(pmvec3_t veca, pmvec3_t vecb, pmvec3_t out)
-{
-	out[0] = veca[0] + vecb[0];
-	out[1] = veca[1] + vecb[1];
-	out[2] = veca[2] + vecb[2];
-}
-
-void _VectorCopy(pmvec3_t in, pmvec3_t out)
-{
-	out[0] = in[0];
-	out[1] = in[1];
-	out[2] = in[2];
-}
-
-#ifndef DISABLE_VEC_FUNCS
 void CrossProduct(const pmvec3_t v1, const pmvec3_t v2, pmvec3_t cross)
 {
 	cross[0] = v1[1] * v2[2] - v1[2] * v2[1];
 	cross[1] = v1[2] * v2[0] - v1[0] * v2[2];
 	cross[2] = v1[0] * v2[1] - v1[1] * v2[0];
 }
-#endif
 
 double sqrt(double x);
 
-#ifndef DISABLE_VEC_FUNCS
 float Length(const pmvec3_t v)
 {
 	int i;
@@ -323,7 +138,6 @@ float Length(const pmvec3_t v)
 
 	return length;
 }
-#endif
 
 float Distance(const pmvec3_t v1, const pmvec3_t v2)
 {
@@ -332,7 +146,6 @@ float Distance(const pmvec3_t v1, const pmvec3_t v2)
 	return Length(d);
 }
 
-#ifndef DISABLE_VEC_FUNCS
 float VectorNormalize(pmvec3_t v)
 {
 	float length, ilength;
@@ -351,80 +164,9 @@ float VectorNormalize(pmvec3_t v)
 	return length;
 }
 
-void VectorInverse(pmvec3_t v)
-{
-	v[0] = -v[0];
-	v[1] = -v[1];
-	v[2] = -v[2];
-}
-
 void VectorScale(const pmvec3_t in, pmvec_t scale, pmvec3_t out)
 {
 	out[0] = in[0] * scale;
 	out[1] = in[1] * scale;
 	out[2] = in[2] * scale;
 }
-#endif
-
-int Q_log2(int val)
-{
-	int answer = 0;
-	while ( val >>= 1 )
-		answer++;
-	return answer;
-}
-
-void VectorMatrix(pmvec3_t forward, pmvec3_t right, pmvec3_t up)
-{
-	pmvec3_t tmp;
-
-	if ( forward[0] == 0 && forward[1] == 0 )
-	{
-		right[0] = 1;
-		right[1] = 0;
-		right[2] = 0;
-		up[0] = -forward[2];
-		up[1] = 0;
-		up[2] = 0;
-		return;
-	}
-
-	tmp[0] = 0;
-	tmp[1] = 0;
-	tmp[2] = 1.0;
-	CrossProduct(forward, tmp, right);
-	VectorNormalize(right);
-	CrossProduct(right, forward, up);
-	VectorNormalize(up);
-}
-
-#ifndef DISABLE_VEC_FUNCS
-void VectorAngles(const pmvec3_t forward, pmvec3_t angles)
-{
-	float tmp, yaw, pitch;
-
-	if ( forward[1] == 0 && forward[0] == 0 )
-	{
-		yaw = 0;
-		if ( forward[2] > 0 )
-			pitch = 90;
-		else
-			pitch = 270;
-	}
-	else
-	{
-		yaw = (atan2(forward[1], forward[0]) * 180 / M_PI);
-		if ( yaw < 0 )
-			yaw += 360;
-
-		tmp = sqrt(forward[0] * forward[0] + forward[1] * forward[1]);
-		pitch = (atan2(forward[2], tmp) * 180 / M_PI);
-		if ( pitch < 0 )
-			pitch += 360;
-	}
-
-	angles[0] = pitch;
-	angles[1] = yaw;
-	angles[2] = 0;
-}
-#endif
